e3d2e2b528f854d0d466660e07cea17d63f238b2
Nakamury1 <sophielamsova29@gmail.com>
2024-09-18 13:10:18 +0200

    JS version finale commenté

--- javascript.js
+++ javascript.js
@@ -1,8 +1,9 @@
+//Utilisation de l'API de taux de change
 function fetchDevise(){
   return fetch(`https://v6.exchangerate-api.com/v6/a710cf5c7f9d8afe873e8a0b/latest/${deviseEntree.value}`)
     .then((response) => response.json())
 }
-
+//Initialisation des variables
 let choice = [ "USD", "AED", "AFN", "ALL", "AMD", "ANG", "AOA", "ARS", "AUD", "AWG", "AZN", "BAM", "BBD", "BDT", "BGN", "BHD", "BIF", "BMD", "BND", "BOB", "BRL", "BSD", "BTN", "BWP", "BYN", "BZD", "CAD", "CDF", "CHF", "CLP", "CNY", "COP", "CRC", "CUP", "CVE", "CZK", "DJF", "DKK", "DOP", "DZD", "EGP", "ERN", "ETB", "EUR", "FJD", "FKP", "FOK", "GBP", "GEL", "GGP", "GHS", "GIP", "GMD", "GNF", "GTQ", "GYD", "HKD", "HNL", "HRK", "HTG", "HUF", "IDR", "ILS", "IMP", "INR", "IQD", "IRR", "ISK", "JEP", "JMD", "JOD", "JPY", "KES", "KGS", "KHR", "KID", "KMF", "KRW", "KWD", "KYD", "KZT", "LAK", "LBP", "LKR", "LRD", "LSL", "LYD", "MAD", "MDL", "MGA", "MKD", "MMK", "MNT", "MOP", "MRU", "MUR", "MVR", "MWK", "MXN", "MYR", "MZN", "NAD", "NGN", "NIO", "NOK", "NPR", "NZD", "OMR", "PAB", "PEN", "PGK", "PHP", "PKR", "PLN", "PYG", "QAR", "RON", "RSD", "RUB", "RWF", "SAR", "SBD", "SCR", "SDG", "SEK", "SGD", "SHP", "SLE", "SLL", "SOS", "SRD", "SSP", "STN", "SYP", "SZL", "THB", "TJS", "TMT", "TND", "TOP", "TRY", "TTD", "TVD", "TWD", "TZS", "UAH", "UGX", "UYU", "UZS", "VES", "VND", "VUV", "WST", "XAF", "XCD", "XDR", "XOF", "XPF", "YER", "ZAR", "ZMW", "ZWL",
 ]
 let sommeC = document.getElementById("somme")
@@ -10,13 +11,15 @@ let deviseEntree = document.getElementById("devise_input")
 let deviseSortie = document.getElementById("devise_output")
 let resultat = document.getElementById("resultat")
 
-function Autocomplete() {
+//Fonction qui permet d'avoir tout les devises dans le menu déroulé 
+function DeviseAuto() {
   for (let i = 0; i < choice.length; i++) {
       deviseEntree.innerHTML += `<option>${choice[i]}</option>`
       deviseSortie.innerHTML += `<option>${choice[i]}</option>`
   }
 }
 
+//Conversion de la somme à convertir
 async function DeviseResult(){
   const data = await fetchDevise()
   let donnees = data.conversion_rates;
@@ -27,7 +30,7 @@ async function DeviseResult(){
   `
 }
 
-Autocomplete()
+DeviseAuto()
 deviseEntree.addEventListener("input", (e) => DeviseResult())
 deviseSortie.addEventListener("input", (e) => DeviseResult())
 sommeC.addEventListener("input", (e) => DeviseResult())
